892e958181c4a208fa7ef8e692737c8de1c70e09
Stefano Veltri <stefano@idrinkritalin.it>
2017-09-03 16:54:00 +0200

    hello world

---
+++ README.md
@@ -0,0 +1,32 @@
+# Movie Finder 🔍 🎬
+App that uses OMDb (Open Movie Database) API's to query movies and show some results.
+
+**Dependencies and some fancy NPM packages used in this project:**
+
+* Node.js - https://nodejs.org
+* Express - https://www.npmjs.com/package/express
+* EJS - https://www.npmjs.com/package/ejs
+* BodyParser - https://www.npmjs.com/package/body-parser
+* Request - https://www.npmjs.com/package/request
+* Open Movie Database API: http://www.omdbapi.com
+
+
+**TO-DOs:**
+
+* Style the project (as it is right now, is *totally not styled*)
+* Receive feedbacks or suggests - thanks for this 🤓
+
+**How to use:**
+
+To install the required dependencies, from the terminal: 
+```
+npm install
+```
+Start the server:
+
+```
+node app.js
+```
+you will be able to listen the server on http://localhost:8080
+
+**Enjoy!**
---
+++ app.js
@@ -0,0 +1,39 @@
+// SETUP
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
+
+// DEFINE ASSETS
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+//// ROUTE
+// GET
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/results", function(req, res){
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?apikey=thewdb&s=" + query;
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var data = JSON.parse(body);
+            res.render("results", {data : data});
+        }  
+    });
+});
+
+app.get("*", function(req, res){
+    res.render("404");
+});
+
+// LISTENING
+app.listen(8080, function(){
+   if(process.env.IP === "0.0.0.0"){
+        process.env.IP = "localhost"
+    }
+    console.log("Server has started on port 8080, my Master!")
+});
